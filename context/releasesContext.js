538dbfff3cf57e41b0ca788fac4922f5b1d3008d
sathish060799 <wimpysathish@gmail.com>
2023-07-23 17:49:12 +0800

    deleted some providers

--- context/releasesContext.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const ReleasesContext = React.createContext([]);
-
-export default ReleasesContext;
--- pages/components/Releases.jsx
+++ pages/components/Releases.jsx
@@ -1,10 +1,7 @@
 import React, {useContext} from 'react'
 import Hero from './Hero'
-import ReleasesContext from '../../context/releasesContext'
 
 export default function Releases() {
-    const newReleases = useContext(ReleasesContext)
-    console.log(newReleases);
     return (
         <div>
             <Hero
